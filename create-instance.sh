db5a93c380e674562645bd03cfc54b6c126b399e
suresh <suresh31346@gmail.com>
2023-08-24 15:37:18 +0530

    added roboshop automation script along with domain record creation

--- create-instance.sh
+++ create-instance.sh
@@ -4,8 +4,35 @@
 echo " Welcome to Automation of roboshop project"
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-088d799746bdd6f9b
+DOMAIN_NAME=testdevopsconcepts.online
 
 for i in "${NAMES[@]}"
 do
-  echo "NAME: $i"
+  if [[ $i == "mongodb" || $i == "mysql" ]]
+  then 
+      INSTANCE_TYPE="t2.micro"
+    else
+       INSTANCE_TYPE="t2.micro"
+   fi    
+  echo "creating $i instance"
+
+  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+
+   echo "created $i instance : $IP_ADDRESS"
+
+   aws route53 change-resource-record-sets --hosted-zone-id Z06567911DNRKHONSDP2K --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
