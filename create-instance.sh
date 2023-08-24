d0d89414d01d1bae77f0dc282bfa285a6a2c8722
suresh <suresh31346@gmail.com>
2023-08-24 14:56:20 +0530

    print required server names

--- create-instance.sh
+++ create-instance.sh
@@ -2,3 +2,10 @@
 
 #This is for creating AWS instances via AWS CommandLine-Automation
 echo " Welcome to Automation of roboshop project"
+
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${NAMES[@]}"
+do
+  echo "NAME: $i"
+done
